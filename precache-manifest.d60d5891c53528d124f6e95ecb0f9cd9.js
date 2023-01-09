7c8e5228602bda97dd92f21729579ea591902250
Code Sandbox <hello@codesandbox.io>
2023-01-09 13:42:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d60d5891c53528d124f6e95ecb0f9cd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47addcd0333170d36ab60f0682f4b8ec",
+    "url": "https://fspillum.github.io/csb-9k4vri/index.html"
+  },
+  {
+    "revision": "dac4b36cedd1083b350c",
+    "url": "https://fspillum.github.io/csb-9k4vri/static/js/2.ab2861a9.chunk.js"
+  },
+  {
+    "revision": "bd02bd5a130719833712",
+    "url": "https://fspillum.github.io/csb-9k4vri/static/js/main.40ea643c.chunk.js"
+  },
+  {
+    "revision": "9fbb5bdafc29f2005726",
+    "url": "https://fspillum.github.io/csb-9k4vri/static/js/runtime-main.b437f338.js"
+  }
+]);
